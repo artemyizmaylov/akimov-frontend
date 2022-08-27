9ef99b8323fb42e302ca1b9cf7e62a0265d63d06
Artemy <artemyizmaylov@gmail.com>
2022-08-27 14:52:26 +0300

    Функции корзины перенесены в хук

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { React, useEffect, useMemo, useState } from "react";
+import { React, useEffect, useState } from "react";
 
-import CartContext from "../../context/CartContext";
+import CartContext, { useCart } from "../../context/CartContext";
 import WindowContext from "../../context/WindowContext";
 
 import Main from "../Main/Main";
@@ -22,88 +22,8 @@ function App() {
   const [filteredItems, setFilteredItems] = useState([]);
   const [lastFilter, setLastFilter] = useState({ id: "", pressCount: 0 });
 
-  const [cartIsOpen, setCartIsOpen] = useState(false);
-  const [cartItems, setCartItems] = useState(
-    JSON.parse(localStorage.getItem("cart")) || []
-  );
-
-  const [totalPrice, setTotalPrice] = useState(0);
+  const cart = useCart();
   const windowWidth = useWindowWidth();
-
-  const countTotalPrice = () => {
-    let total = 0;
-
-    if (cartItems.length > 0) {
-      cartItems.forEach((item) => {
-        total += item.price * item.count;
-      });
-    }
-
-    return total;
-  };
-
-  const increaseItemCount = (item) => {
-    const index = cartItems.findIndex(
-      (cartItem) =>
-        cartItem.article === item.article &&
-        cartItem.size === item.size &&
-        cartItem.material === item.material
-    );
-    const newItems = [...cartItems];
-
-    newItems[index].count = item.count + 1;
-    setCartItems(newItems);
-  };
-
-  const decreaseItemCount = (item) => {
-    const index = cartItems.findIndex(
-      (cartItem) =>
-        cartItem.article === item.article &&
-        cartItem.size === item.size &&
-        cartItem.material === item.material
-    );
-
-    if (index > -1) {
-      const newItems = [...cartItems];
-
-      newItems[index].count = item.count - 1;
-      setCartItems(newItems);
-    }
-  };
-
-  const addToCart = (item) => {
-    const index = cartItems.findIndex(
-      (cartItem) =>
-        cartItem.article === item.article &&
-        cartItem.size === item.size &&
-        cartItem.material === item.material
-    );
-
-    const itemCopy = { ...item };
-    itemCopy.count = 1;
-
-    if (index === -1) {
-      setCartItems([...cartItems, itemCopy]);
-    } else {
-      increaseItemCount(itemCopy);
-    }
-  };
-
-  const deleteFromCart = (item) => {
-    const index = cartItems.findIndex(
-      (cartItem) =>
-        cartItem.article === item.article &&
-        cartItem.size === item.size &&
-        cartItem.material === item.material
-    );
-
-    if (index > -1) {
-      const newItems = [...cartItems];
-
-      newItems.splice(index, 1);
-      setCartItems(newItems);
-    }
-  };
 
   const search = (id) => {
     switch (id) {
@@ -159,25 +79,10 @@ function App() {
     }
   };
 
-  const cartContext = useMemo(
-    () => ({
-      cartIsOpen,
-      setCartIsOpen,
-      cartItems,
-      setCartItems,
-      addToCart,
-      increaseItemCount,
-      decreaseItemCount,
-      deleteFromCart,
-      totalPrice,
-    }),
-    [cartIsOpen, cartItems, totalPrice]
-  );
-
   useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cartItems));
-    setTotalPrice(countTotalPrice());
-  }, [cartItems]);
+    localStorage.setItem("cart", JSON.stringify(cart.cartItems));
+    cart.setTotalPrice(cart.countTotalPrice());
+  }, [cart.cartItems]);
 
   useEffect(() => {
     getItems().then((data) => {
@@ -190,7 +95,7 @@ function App() {
   return (
     <div className="App">
       <WindowContext.Provider value={windowWidth}>
-        <CartContext.Provider value={cartContext}>
+        <CartContext.Provider value={cart}>
           <Routes>
             <Route exact path="/" element={<Main />} />
             <Route
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,4 +1,106 @@
-import { createContext } from 'react';
+import { createContext, useState, useMemo } from "react";
 
 const CartContext = createContext();
 export default CartContext;
+
+function useCart() {
+  const [cartIsOpen, setCartIsOpen] = useState(false);
+  const [cartItems, setCartItems] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const countTotalPrice = () => {
+    let total = 0;
+
+    if (cartItems.length > 0) {
+      cartItems.forEach((item) => {
+        total += item.price * item.count;
+      });
+    }
+
+    return total;
+  };
+
+  const increaseItemCount = (item) => {
+    const index = cartItems.findIndex(
+      (cartItem) =>
+        cartItem.article === item.article &&
+        cartItem.size === item.size &&
+        cartItem.material === item.material
+    );
+    const newItems = [...cartItems];
+
+    newItems[index].count = item.count + 1;
+    setCartItems(newItems);
+  };
+
+  const decreaseItemCount = (item) => {
+    const index = cartItems.findIndex(
+      (cartItem) =>
+        cartItem.article === item.article &&
+        cartItem.size === item.size &&
+        cartItem.material === item.material
+    );
+
+    if (index > -1) {
+      const newItems = [...cartItems];
+
+      newItems[index].count = item.count - 1;
+      setCartItems(newItems);
+    }
+  };
+
+  const addToCart = (item) => {
+    const index = cartItems.findIndex(
+      (cartItem) =>
+        cartItem.article === item.article &&
+        cartItem.size === item.size &&
+        cartItem.material === item.material
+    );
+
+    const itemCopy = { ...item };
+    itemCopy.count = 1;
+
+    if (index === -1) {
+      setCartItems([...cartItems, itemCopy]);
+    } else {
+      increaseItemCount(itemCopy);
+    }
+  };
+
+  const deleteFromCart = (item) => {
+    const index = cartItems.findIndex(
+      (cartItem) =>
+        cartItem.article === item.article &&
+        cartItem.size === item.size &&
+        cartItem.material === item.material
+    );
+
+    if (index > -1) {
+      const newItems = [...cartItems];
+
+      newItems.splice(index, 1);
+      setCartItems(newItems);
+    }
+  };
+
+  return useMemo(
+    () => ({
+      cartIsOpen,
+      setCartIsOpen,
+      cartItems,
+      setCartItems,
+      addToCart,
+      increaseItemCount,
+      decreaseItemCount,
+      deleteFromCart,
+      totalPrice,
+      setTotalPrice,
+      countTotalPrice,
+    }),
+    [cartIsOpen, cartItems, totalPrice]
+  );
+}
+
+export { useCart };
